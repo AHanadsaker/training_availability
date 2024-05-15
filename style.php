2b739e1bcfa381fd265eac147840e59f02956db2
Alex <43963205+AHanadsaker@users.noreply.github.com>
2024-05-14 22:11:35 -0500

    Add files via upload
    Styling changes

---
+++ style.php
@@ -0,0 +1,44 @@
+<title>vZLC - Training Availability</title>
+<script src="https://kit.fontawesome.com/c8a5a8f1cf.js" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<style>
+#customers {
+  border-collapse: collapse;
+}
+
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #f5f2df;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #263940;
+  color: white;
+}
+
+body {
+  background-image: url('https://media.wired.com/photos/5932b00c9be5e55af6c26844/191:100/w_1280,c_limit/IMG_0938.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed; 
+  background-size: 100% 100%;
+  font-family: Georgia, serif;
+}
+
+.center {
+  text-align: center;
+  background-color: white;
+  margin: auto;
+  width: 60%;
+  border: 1px solid black;
+  border-radius: 15px;
+  padding: 10px;
+  opacity: 0.85;
+}
+</style>
