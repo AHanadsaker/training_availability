641c266fb8f62fea97072c58e5a8023ad56ca75c
AHanadsaker <alex.handsaker@gmail.com>
2024-05-16 18:23:35 -0500

    Roll-back git test

--- cancel.php
+++ cancel.php
@@ -15,7 +15,6 @@ if(mysqli_query($conn, $sql)) {
     
     if(mysqli_num_rows($result) > 0) {
         while($row = mysqli_fetch_assoc($result)) {
-            echo "test";
         $student_cid = $row["student_cid"];
         $start_unix = $row["start"];
         $end_unix = $row["end"];
